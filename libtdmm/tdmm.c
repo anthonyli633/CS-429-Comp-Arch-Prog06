a1c64b9c3cb477ef7834f28cf8dc0ee1d1540ef9
Anthony Li <lanthony@cs.utexas.edu>
2026-02-25 16:43:48 -0600

    Initial Commit

---
+++ libtdmm/tdmm.c
@@ -0,0 +1,14 @@
+#include "tdmm.h"
+
+void t_init(alloc_strat_e strat) {
+	// TODO: Implement this
+}
+
+void *t_malloc(size_t size) {
+	// TODO: Implement this
+	return NULL;
+}
+
+void t_free(void *ptr) {
+  	// TODO: Implement this
+}
